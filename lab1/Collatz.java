7ddfb95e7cfe4a2edea9bf2e87768dc3aedf92d7
1slim2 <yuruize05@gmail.com>
2025-06-24 19:48:30 +0800

    revise with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -10,7 +10,7 @@ public class Collatz {
             return n;
         }else if(n%2==0)
         {
-            return n/2;
+            return n/4;
         }else{
             return 3*n + 1;
         }
